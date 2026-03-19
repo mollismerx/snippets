846b0ca7780fd6e7e1432d1fc2685c67eb5015b5
Robin Nicholson <mollis.merx@gmail.com>
2026-03-19 22:19:33 +0000

    remember this

---
+++ GoogleColab/auto-shutdown.py
@@ -0,0 +1,8 @@
+# Add this to your last cell
+from google.colab import runtime
+
+# Perform your final save/export here
+# model.save('my_model.h5')
+
+print("Mission accomplished. Shutting down to save credits.")
+runtime.unassign()
